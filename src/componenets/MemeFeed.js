c5fe1eaa47f50775713a346d86b31fc457b2d455
SupremeAmer <emmyplumbingservice@gmail.com>
2025-06-02 22:29:23 +0100

    Add files via upload

--- src/componenets/MemeFeed.js
+++ src/componenets/MemeFeed.js
@@ -1,1 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { databases, storage } from "../appwrite/config";
+import { Card, CardMedia, CardContent, Typography, Box } from "@mui/material";
 
+function MemeFeed() {
+  const [memes, setMemes] = useState([]);
+
+  useEffect(() => {
+    const fetchMemes = async () => {
+      try {
+        const res = await databases.listDocuments(
+          process.env.REACT_APP_APPWRITE_DATABASE_ID,
+          process.env.REACT_APP_APPWRITE_MEMES_COLLECTION_ID
+        );
+        setMemes(res.documents.reverse());
+      } catch (err) {
+        setMemes([]);
+      }
+    };
+    fetchMemes();
+  }, []);
+
+  return (
+    <Box>
+      {memes.map(meme => (
+        <Card key={meme.$id} sx={{ mb: 2 }}>
+          <CardMedia
+            component="img"
+            height="240"
+            image={storage.getFilePreview(process.env.REACT_APP_APPWRITE_BUCKET_ID, meme.fileId)}
+            alt={meme.caption}
+          />
+          <CardContent>
+            <Typography>{meme.caption}</Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </Box>
+  );
+}
+
+export default MemeFeed;
